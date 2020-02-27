24cdb6c5a0f2e8e3faa48add75b5f89486394b8c
26968986 <nggimseng@gmail.com>
2020-02-27 16:06:02 +0000

    added chap 5

--- Chapter 5: Statistics/stats.py
+++ Chapter 5: Statistics/stats.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+from collections import Counter, defaultdict
 
+
+num_friends=[1,1,1,2,3,4,4,4,4,6,1,1,1,2,10]
+friend_counts=Counter(num_friends)
+xs=range(max(num_friends))
+ys = [friend_counts[x] for x in xs]
+plt.bar(xs,ys)
+plt.axis([0,max(num_friends)+1,0,25])
+plt.show()
